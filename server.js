c639c979c962098df620dbb1eee01f3f3cb5eab1
Valay Andhariya <andhariyavalay4@gmail.com>
2024-05-10 00:57:00 +0530

    Adding SSL certificate

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
 import { app } from "./app.js";
 import { connDb } from "./data/database.js";
 import { config } from "dotenv";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import https from "https";
+import fs from "fs";
+import path from "path";
+
 const port = 5000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const sslServer = https.createServer(
+  {
+    key: fs.readFileSync(path.join(__dirname, "cert", "key.pem")),
+    cert: fs.readFileSync(path.join(__dirname, "cert", "cert.pem")),
+  },
+  app
+);
 
 connDb();
 
-app.listen(port, () => {
+sslServer.listen(port, () => {
   console.log(`server is working! on port ${port}`);
 });
 
